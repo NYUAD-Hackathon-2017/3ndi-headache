245742a55ae1831fc628c93e8edabaa5bf80fad3
Daniel Watson <watsondaniel6@gmail.com>
2017-04-15 16:21:19 +0400

    Working phone call stream

--- main.py
+++ main.py
@@ -1,38 +1,63 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
 
+import tornado.httpserver
+import tornado.websocket
 import tornado.ioloop
 import tornado.web
-import tornado.websocket
 
 
-class MainHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.write("Hello, world")
-
-
-class EchoWebSocket(tornado.websocket.WebSocketHandler):
+class WSHandler(tornado.websocket.WebSocketHandler):
+  connections = []
+  
   def check_origin(self, origin):
     return True
   
   def open(self):
-    print("WebSocket opened")
-
+    print("Client connected")
+    # Add the connection to the list of connections
+    self.connections.append(self)
+  
   def on_message(self, message):
-    self.write_message(u'You said: ' + message)
+    # Check if message is binary or text
+    if type(message) == str:
+      print(type(message), len(message))
+      # Echo the binary message back to where it came from
+      self.write_message(message, binary=True)
+    else:
+      print("Message received: ", message)
+      self.write_message("Your message: " + message)
   
   def on_close(self):
-    print("WebSocket closed")
+    # Remove the connection from the list of connections
+    self.connections.remove(self)
+    print("Client disconnected")
 
 
-def make_app():
-  return tornado.web.Application([
-    (r'/', MainHandler),
-    (r'/socket', EchoWebSocket),
-  ])
+class NCCOHandler(tornado.web.RequestHandler):
+  def get(self):
+    print('GET:', self.request.body)
+    with open('ncco.json', 'r') as f:
+      ncco = f.read()
+    self.write(ncco)
+    self.set_header('Content-Type', 'application/json')
+    self.finish()
+  
+  def post(self):
+    print('POST:', self.request.body)
+    self.finish()
 
 
-if __name__ == "__main__":
-  print("Starting app in http://localhost:3000")
-  app = make_app()
-  app.listen(3000)
-  tornado.ioloop.IOLoop.current().start()
+# Router
+application = tornado.web.Application([
+  (r'/socket', WSHandler),
+  (r'/', NCCOHandler),
+])
+
+
+if __name__ == '__main__':
+  print("Starting server in port 3000")
+  http_server = tornado.httpserver.HTTPServer(application)
+  http_server.listen(3000)
+  tornado.ioloop.IOLoop.instance().start()
